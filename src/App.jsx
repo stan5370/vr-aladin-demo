e3d653860f6ca640d23b6c0adff971144d2bc36a
Benjamin Ye <benjaminye.email@gmail.com>
2025-10-04 17:00:04 -0500

    this might be great. or it might be the worst change ever

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,27 @@
+import { Canvas } from '@react-three/fiber'
+import { XR, createXRStore } from '@react-three/xr'
 import Aladin from './components/aladin'
+
+const store = createXRStore()
 
 function App() {
   return (
-    <Aladin 
-      target={"18 03 57.94 -28 40 55.0"}
-      projection={"STG"}
-      survey={"CDS/P/Mellinger/color"}
-      fullScreen={true}
-      showSimbadPointerControl={true}
-      showReticle={false}
-      fov={60}
-      realFullscreen={true}
-    />
-  )
-}
+    <>
+      <Aladin 
+        target={"18 03 57.94 -28 40 55.0"}
+        projection={"STG"}
+        survey={"CDS/P/Mellinger/color"}
+        fullScreen={true}
+        showSimbadPointerControl={true}
+        showReticle={false}
+        fov={60}
+        realFullscreen={true}
+      />
+      <Canvas>
+        <XR store={store}>
+        </XR>
+      </Canvas>
+    </>
+  )}
 
 export default App
