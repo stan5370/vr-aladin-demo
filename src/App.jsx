251f6a61fd8c696a8adfa2084961470672f3236f
Benjamin Ye <benjaminye.email@gmail.com>
2025-10-05 02:09:37 -0500

    we have something

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import * as THREE from "three"
 import { Canvas } from '@react-three/fiber'
 import { XR, createXRStore } from '@react-three/xr'
 import { Html, Environment, PerspectiveCamera } from '@react-three/drei'
@@ -8,7 +9,6 @@ const store = createXRStore()
 function App() {
     return (
         <>
-            <Aladin />
             <Canvas
                 style={{
                     position: "fixed",
@@ -20,15 +20,15 @@ function App() {
                     <PerspectiveCamera makeDefault position={[0, 1.6, 2]} fov={60} />
                     <Environment preset="warehouse" />
                     <mesh>
-                        <sphereGeometry args={[5000, 16, 16, Math.PI / 2, Math.PI * 2, 0, Math.PI]} />
-                        <meshStandardMaterial color="green" />
-                    </mesh>
-                    <mesh rotation-x={-Math.PI / 2}>
-                        <planeGeometry args={[6, 6]} />
-                        <meshStandardMaterial color="white" />
-                        <Html>
+                        <sphereGeometry args={[50, 32, 16, Math.PI / 2, Math.PI * 2, 0, Math.PI]} />
+                        <meshStandardMaterial side={THREE.DoubleSide} />
+                        <Html position={[0, -0.1, 0]} transform>
                             <Aladin />
                         </Html>
+                    </mesh>
+                    <mesh rotation-x={-Math.PI / 2}>
+                        <circleGeometry args={[2.5, 32]} />
+                        <meshStandardMaterial color="gray" />
                     </mesh>
                 </XR>
             </Canvas>
