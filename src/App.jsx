7e9ac69435578dca34570a0bf8f046e7b5a1e3f8
Benjamin Ye <benjaminye.email@gmail.com>
2025-10-05 01:44:43 -0500

    testing in prod again'

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,39 @@
 import { Canvas } from '@react-three/fiber'
 import { XR, createXRStore } from '@react-three/xr'
-import { Html } from '@react-three/drei'
+import { Html, Environment, PerspectiveCamera } from '@react-three/drei'
 import Aladin from './components/aladin'
 
-function App() {
-  const store = createXRStore()
+const store = createXRStore()
 
-  return (
-    <Canvas>
-      <XR store={store}>
-        <Html>
-          <div onPointerDown={(e) => e.stopPropagation()}>
-            <Aladin 
-              target={"18 03 57.94 -28 40 55.0"}
-              projection={"STG"}
-              survey={"CDS/P/Mellinger/color"}
-              fullScreen={true}
-              showSimbadPointerControl={true}
-              showReticle={false}
-              fov={60}
-              realFullscreen={true}
-            />
-          </div>
-        </Html>
-      </XR>
-    </Canvas>
-  )
+function App() {
+    return (
+        <>
+            <Aladin />
+            <Canvas
+                style={{
+                    position: "fixed",
+                    width: "100vw",
+                    height: "100vh",
+                }}
+            >
+                <PerspectiveCamera makeDefault position={[0, 1.6, 2]} fov={60} />
+                <Environment preset="warehouse" />
+                <mesh>
+                    <sphereGeometry args={[5000, 16, 16, Math.PI / 2, Math.PI * 2, 0, Math.PI]} />
+                    <meshStandardMaterial color="green" />
+                </mesh>
+                <mesh rotation-x={-Math.PI / 2}>
+                    <planeGeometry args={[6, 6]} />
+                    <meshStandardMaterial color="white" />
+                    <Html>
+                        <Aladin />
+                    </Html>
+                </mesh>
+                <XR store={store}>
+                </XR>
+            </Canvas>
+        </>
+    )
 }
 
 export default App
--- src/components/aladin.jsx
+++ src/components/aladin.jsx
@@ -1,52 +1,59 @@
 import { useEffect, useRef } from "react";
-import useWindowDimensions from "./windowDimensions";
 
-export default function Aladin(props) {
-  const aladinRef = useRef();
-  const { height, width } = useWindowDimensions();
+export default function Aladin() {
+    const aladinRef = useRef();
+    const height = window.height;
+    const width = window.height;
 
-  useEffect(() => {
-    const loadAladin = () => {
-      // Initialize catalog
-      const catalog = window.A.catalogHiPS("https://hipscat.cds.unistra.fr/HiPSCatService/Simbad", {
-        onClick: (source) => {
-          console.log("Clicked source:", source);
-        },
-      });
+    useEffect(() => {
+        const loadAladin = () => {
+            // Initialize catalog
+            const catalog = window.A.catalogHiPS("https://hipscat.cds.unistra.fr/HiPSCatService/Simbad", {
+                onClick: (source) => {
+                    console.log("Clicked source:", source);
+                },
+            });
 
-      // Create aladin
-      const aladin = window.A.aladin(aladinRef.current, {...props});
-      aladin.addCatalog(catalog);
+            // Create aladin
+            const aladin = window.A.aladin('#aladin-lite-div', {
+                target: "18 03 57.94 -28 40 55.0",
+                projection: "STG",
+                survey: "CDS/P/Mellinger/color",
+                fullScreen: true,
+                showSimbadPointerControl: true,
+                showReticle: false,
+                fov: 60,
+            });
+            aladin.addCatalog(catalog);
+            aladinRef.current = aladin;
+        };
 
-      return aladin;
-    };
+        // Load Aladin script dynamically if not already loaded
+        const existingScript = document.getElementById("aladin-script");
 
-    // Load Aladin script dynamically if not already loaded
-    const existingScript = document.getElementById("aladin-script");
+        if (!existingScript) {
+            const script = document.createElement("script");
+            script.id = "aladin-script";
+            script.src =
+                "https://aladin.cds.unistra.fr/AladinLite/api/v3/latest/aladin.js";
+            script.async = true;
 
-    if (!existingScript) {
-      const script = document.createElement("script");
-      script.id = "aladin-script";
-      script.src =
-        "https://aladin.cds.unistra.fr/AladinLite/api/v3/latest/aladin.js";
-      script.async = true;
+            script.onload = () => {
+                if (window.A) {
+                    window.A.init.then(loadAladin);
+                }
+            };
 
-      script.onload = () => {
-        if (window.A) {
-          window.A.init.then(loadAladin);
+            document.body.appendChild(script);
+        } else {
+            if (window.A) {
+                window.A.init.then(loadAladin);
+            }
         }
-      };
+    }, []);
 
-      document.body.appendChild(script);
-    } else {
-      if (window.A) {
-        window.A.init.then(loadAladin);
-      }
-    }
-  }, [props]);
-
-  return (
-    <div ref={aladinRef} id="aladin-lite-div" style={{ width: width, height: height }} />
-  );
+    return (
+        <div ref={aladinRef} id="aladin-lite-div" style={{ width: width, height: height }} />
+    );
 }
 
